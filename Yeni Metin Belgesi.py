9901f64a4634b3b021aa9c5f6ec9ac49fb57bd74
SAVAŞ METİNOĞLU <76447218+Savasmetin@users.noreply.github.com>
2020-12-27 23:05:48 +0300

    Add files via upload

---
+++ Yeni Metin Belgesi.py
@@ -0,0 +1,18 @@
+string="ad","soyad"
+ad=input("adınız:")
+soyad=input("soyadınız:")
+dgm=int(input("dogum yılınız:"))
+yas = 2020 -  dgm
+print(yas)
+list1=(ad,soyad,dgm,yas)
+print(list1)
+
+for x in list1:
+
+   print(x)
+   if  0< yas<18:
+
+      print(" yasak var çıkma")
+
+   else:
+      print("dışarı çıkabilirsin ")
